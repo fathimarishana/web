06be4d67b67b2778a93f3b81227e6801391ed960
retyh <hjkh@example.com>
2024-12-03 15:10:40 +0530

    html

---
+++ base1.js
@@ -0,0 +1,14 @@
+<html>
+<head>
+<script>
+function displaymessage(){
+console.log("helloworld");
+}
+</script>
+</head>
+<body>
+<form>
+<input type="button" value="click me" onClick="displaymessage()">
+</form>
+</body>
+</html>
